c9d04258c0610ca55ce599cf20edfbcb22033c75
Aleksandar Cvetković <office@e-seo.info>
2026-01-23 19:59:10 +0100

    feat(header): add placeholder for last update date bellow main headline

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,9 +4,12 @@ import ThemeSwitch from "./ThemeSwitch";
 const Header = () => {
   return (
     <header data-testid="header" className="w-full h-16 border-b border-border bg-card flex items-center justify-between px-4 shrink-0">
+      <div className="flex flex-col items-center justify-center gap-0 w-full mb-0">
       <h1 className="font-semibold mx-auto" style={{ fontSize: 'clamp(0.875rem, 2vw + 0.5rem, 1.25rem)' }}>
         <Link to="/">Saobraćajne nesreće Vlasotince</Link>
       </h1>
+        <span className="d-block text-[12px] text-muted-foreground">Poslednji update: 23.01.2026.</span>
+      </div>
       <ThemeSwitch />
     </header>
   );
